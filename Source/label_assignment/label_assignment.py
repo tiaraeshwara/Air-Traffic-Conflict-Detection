796385e43e16fcb0044b198e8926310a435c04fc
AjanEshwara <ajan.eshwara@gmail.com>
2025-08-08 02:08:54 +0100

    label assign updated

--- Source/label_assignment/label_assignment.py
+++ Source/label_assignment/label_assignment.py
@@ -6,22 +6,6 @@ def assign_si_label(df,
                     vert_sep_col='vertical_sep_ft',
                     horiz_threshold=10.0,   # nautical miles
                     vert_threshold=1000.0): # feet
-    """
-    Assign binary Situation of Interest (SI) label to each aircraft pair based on
-    separation thresholds:
-        SI = 1 if horizontal separation < horiz_threshold AND vertical separation < vert_threshold
-        Else, 0.
-
-    Args:
-        df (pd.DataFrame): DataFrame containing relative feature columns.
-        horiz_sep_col (str): Column name for horizontal separation in NM.
-        vert_sep_col (str): Column name for vertical separation in feet.
-        horiz_threshold (float): Horizontal threshold for SI in NM.
-        vert_threshold (float): Vertical threshold for SI in feet.
-
-    Returns:
-        pd.DataFrame: Same DataFrame with an added 'SI' column.
-    """
 
     # Verify required columns exist
     if horiz_sep_col not in df.columns or vert_sep_col not in df.columns:
@@ -31,15 +15,13 @@ def assign_si_label(df,
     print(f"Assigned SI labels: {df['SI'].value_counts().to_dict()}")
     return df
 
-def main():
-    INPUT_FEATURES = '../../data/processed/features/features_segment_0.csv'  # Adjust path
-    OUTPUT_LABELED = '../../data/processed/labeled/labeled_segment_0.csv'
+def label_and_save(OUTPUT_PATH,OUTPUT_LABELED):
 
     # Ensure output directory exists
     os.makedirs(os.path.dirname(OUTPUT_LABELED), exist_ok=True)
 
     # Load features dataset
-    df_features = pd.read_csv(INPUT_FEATURES)
+    df_features = pd.read_csv(OUTPUT_PATH)
     print(f"Loaded features data with shape: {df_features.shape}")
 
     # Assign SI labels
@@ -52,5 +34,3 @@ def main():
     # Preview
     print(df_labeled.head())
 
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Source.data_processing.process_and_combine import process_all_and_save
 from Source.data_processing.segment_data import segment_adsb_data
 from Source.data_processing.pair_generate import proceed_pairgeneration
-
 from Source.feature_engineering.feature_engineering import load_and_process_all_pairs
+from Source.label_assignment.label_assignment import label_and_save
 
 import os
 import pandas as pd
@@ -31,5 +31,7 @@ proceed_pairgeneration()
 if __name__ == "__main__":
     INPUT_PAIRS = 'Data/processed_data/pairs/'
     OUTPUT_PATH = 'Data/processed_data/features/features_eng.csv'
+    OUTPUT_LABELED ='Data/processed_data/labeled/labeled_data.csv'
 
     load_and_process_all_pairs(INPUT_PAIRS, OUTPUT_PATH)
+    label_and_save(OUTPUT_PATH, OUTPUT_LABELED)
